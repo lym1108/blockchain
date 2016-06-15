f1344a6df090e3c401d73cd130c9694431fcded8
haozhouxu <470001421@qq.com>
2016-06-15 17:49:27 +0800

    修改index和center和冻结界面

--- public/applyUnfreeze.js
+++ public/applyUnfreeze.js
@@ -1,5 +1,8 @@
+var currentTime;
+
 window.onload = function () {
-	$(timeleft2).html(transferTime($(timeleft).text()));
+	currentTime = parseInt($(timeleft).text());
+	transferTime();
 
 
 };
@@ -15,15 +18,19 @@ function fgoback() {
 	window.location.href = "/person?addr=" + GetQueryString("addr");
 }
 
-function transferTime(timeValue) {
-	var theTime = parseInt(timeValue);
+function transferTime() {
+	var theTime = currentTime;
 	var dayCount = Math.floor(theTime / (60 * 60 * 24));
 	var dayLeft = theTime % (60 * 60 * 24);
 	var hourCount = Math.floor(dayLeft / (60 * 60));
-	var hourLeft = hourCount % (60 * 60);
+	var hourLeft = dayLeft % (60 * 60);
 	var minuteCount = Math.floor(hourLeft / (60));
 	var secondCount = hourLeft % (60);
+	
+	$(timeleft2).html(dayCount + "天" + hourCount + "小时" + minuteCount + "分钟" + secondCount + "秒");
+	currentTime = currentTime-1;
+	t=setTimeout("transferTime()",1000);
 
 	// return dayCount + "天" + hourCount + "小时" + minuteCount + "分钟" + secondCount + "秒";
-	return dayCount + "天" + hourCount + "小时" + minuteCount + "分钟";
+	// return dayCount + "天" + hourCount + "小时" + minuteCount + "分钟";
 }
