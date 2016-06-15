d35df259ec521a8b8e329cda83aa22c7ddb2a3a7
haozhouxu <470001421@qq.com>
2016-06-15 14:40:48 +0800

    修改质押和解冻界面

--- public/applyUnfreeze.js
+++ public/applyUnfreeze.js
@@ -1,17 +1,29 @@
-window.onload = function() {
+window.onload = function () {
+	$(timeleft2).html(transferTime($(timeleft).text()));
 
 
-	
 };
 
-function GetQueryString(name)
-{var reg= new RegExp("(^|&)"+name+"=([^&]*)(&|$)");
-var r=window.location.search.substr(1).match(reg);
-if(r!=null)
-return unescape(r[2]);
-return ""; 
+function GetQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null)
+		return unescape(r[2]);
+	return "";
 }
-function fgoback(){
-	 window.location.href="/person?addr="+GetQueryString("addr");
+function fgoback() {
+	window.location.href = "/person?addr=" + GetQueryString("addr");
 }
 
+function transferTime(timeValue) {
+	var theTime = parseInt(timeValue);
+	var dayCount = Math.floor(theTime / (60 * 60 * 24));
+	var dayLeft = theTime % (60 * 60 * 24);
+	var hourCount = Math.floor(dayLeft / (60 * 60));
+	var hourLeft = hourCount % (60 * 60);
+	var minuteCount = Math.floor(hourLeft / (60));
+	var secondCount = hourLeft % (60);
+
+	// return dayCount + "天" + hourCount + "小时" + minuteCount + "分钟" + secondCount + "秒";
+	return dayCount + "天" + hourCount + "小时" + minuteCount + "分钟";
+}
